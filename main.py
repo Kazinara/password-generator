f228c3c57a7eea3425b703f7dfbe1a64816041df
Kazinara <anastasiakorolevskaya02@gmail.com>
2025-07-14 20:05:45 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from random import choice
+from string import printable
+all_characters = list(printable.strip())
+status = False
+while not status:
+    length_pass = int(input('Введите длину пароля: '))
+    if 7 <= length_pass <= 20:
+        print('Длина пароля подходящая. Начинаем генерировать пароль...')
+        status = True
+    else:
+        print('Длина пароля должна быть от 7 до 20 символов')
+while status: 
+    password = ''.join([choice(all_characters) for number in range(length_pass)])
+    print(f'Сгенерированный пароль: {password}')
+    us_answ = input('Нравится ли вам пароль? (да/нет) ').lower()
+    if us_answ == 'нет':
+        print('Давайте сгенерируем другой пароль')
+    elif us_answ == 'да':
+        status = False
+        print(f'Поздравляем! Ваш новый пароль: {password}')
+    else:
+        print('Остановка программы. Запрос не найден...')
+        break
+    
+
+
+
+
+
+    
