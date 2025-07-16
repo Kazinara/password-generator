20f48a4803a9c58d135368ab7bd6c3ab6237cf26
Kazinara <anastasiakorolevskaya02@gmail.com>
2025-07-16 14:37:36 +0300

    Update main.py
    Использовала срезы строк для удаление лишних символов, импортировала библиотеку time и применила метод .sleep.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import time
 from random import choice
 from string import printable
-all_characters = list(printable.strip())
+all_characters = list(printable)
+num = len(all_characters)
+num_one = num - 5
+all_characters = all_characters[:num_one]
 status = False
 while not status:
     length_pass = int(input('Введите длину пароля: '))
     if 7 <= length_pass <= 20:
         print('Длина пароля подходящая. Начинаем генерировать пароль...')
+        time.sleep(2)
         status = True
     else:
         print('Длина пароля должна быть от 7 до 20 символов')
@@ -14,7 +19,8 @@ while status:
     print(f'Сгенерированный пароль: {password}')
     us_answ = input('Нравится ли вам пароль? (да/нет) ').lower()
     if us_answ == 'нет':
-        print('Давайте сгенерируем другой пароль')
+        print('Давайте сгенерируем другой пароль...')
+        time.sleep(2)
     elif us_answ == 'да':
         status = False
         print(f'Поздравляем! Ваш новый пароль: {password}')
